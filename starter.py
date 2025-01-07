1a30ed26b6df299a034016950556f10dae279e16
vscode <vscode@qq.com>
2025-01-07 06:23:52 +0000

    10

--- starter.py
+++ starter.py
@@ -4,6 +4,8 @@ import traceback
 import chardet
 import importlib.util
 
+script_name = 'python_code'
+
 
 def get_executable_dir():
     # 检查是否为打包后的可执行文件
@@ -22,18 +24,17 @@ def replace_main_with_index(input_file, output_file):
         encoding = result['encoding']
     with open(input_file, 'r', encoding=encoding.lower()) as file:
         data = file.read()
-    new_data = data.replace("__name__ == '__main__'", "__name__ == 'python_code'")
+    new_data = data.replace("__name__ == '__main__'", f"__name__ == '{script_name}'")
     # 将替换后的内容写入输出文件
     with open(output_file, 'w', encoding='utf-8') as file:
         file.write(new_data)
 
 
 def load_and_run_main_py():
-    script_name = 'python_code'
     script_path = os.path.join(get_executable_dir(), script_name)
     if os.path.exists(script_path + ".txt"):
         replace_main_with_index(script_path + ".txt", script_path + ".py")
-        spec = importlib.util.spec_from_file_location("index", script_path)
+        spec = importlib.util.spec_from_file_location(script_name, script_path + ".py")
         index_module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(index_module)
     else:
